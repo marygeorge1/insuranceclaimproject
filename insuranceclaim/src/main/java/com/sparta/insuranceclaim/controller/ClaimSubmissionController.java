1a192c70749312eae4a5963e86663affbb3dac12
marygeorge <marygeorge.ed@gmail.com>
2024-01-09 12:18:04 +0000

    added bootstrap links

--- insuranceclaim/src/main/java/com/sparta/insuranceclaim/controller/ClaimSubmissionController.java
+++ insuranceclaim/src/main/java/com/sparta/insuranceclaim/controller/ClaimSubmissionController.java
@@ -1,7 +1,14 @@
 package com.sparta.insuranceclaim.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ClaimSubmissionController {
+
+    @GetMapping("/claimform")
+    public String showClaimForm(){
+        return "claimform";
+    }
 }
