46831f3e0c061584a5d702bcf8dbe1db98518935
Alin Rusu <ARusu@spartaglobal.com>
2024-01-10 12:52:26 +0000

    added generated refference no added methods to claim service for adding a new claim

--- insuranceclaim/src/main/java/com/sparta/insuranceclaim/controller/ClaimSubmissionController.java
+++ insuranceclaim/src/main/java/com/sparta/insuranceclaim/controller/ClaimSubmissionController.java
@@ -3,6 +3,7 @@ package com.sparta.insuranceclaim.controller;
 import com.sparta.insuranceclaim.model.Claim;
 import com.sparta.insuranceclaim.repository.ClaimRepository;
 import com.sparta.insuranceclaim.repository.UserRepository;
+import com.sparta.insuranceclaim.service.ClaimService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ClaimSubmissionController {
     private  ClaimRepository claimRepository;
     private  UserRepository userRepository;
+private ClaimService claimService;
 
-    public ClaimSubmissionController(ClaimRepository claimRepository, UserRepository userRepository) {
+    public ClaimSubmissionController(ClaimRepository claimRepository, UserRepository userRepository, ClaimService claimService) {
         this.claimRepository = claimRepository;
         this.userRepository = userRepository;
+        this.claimService = claimService;
     }
+
     @GetMapping("/claim/create")
     public String getCreateForm(){
         return "claimform";
     }
     @PostMapping("/claim/create")
     public String createClaim(@ModelAttribute() Claim claim){
-        claimRepository.save(claim);
+        claimService.addClaim(claim);
         return "claimform";
     }
 }
