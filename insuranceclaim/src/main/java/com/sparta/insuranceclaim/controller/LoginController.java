2f0ef1efe716718804b52436cc7d72b172c0616f
valgaudeau <valgaudeau@outlook.com>
2024-01-11 11:18:46 +0000

    first working sweetalert notification

--- insuranceclaim/src/main/java/com/sparta/insuranceclaim/controller/LoginController.java
+++ insuranceclaim/src/main/java/com/sparta/insuranceclaim/controller/LoginController.java
@@ -1,13 +1,24 @@
 package com.sparta.insuranceclaim.controller;
 
+import com.sparta.insuranceclaim.model.Claim;
+import com.sparta.insuranceclaim.model.User;
+import com.sparta.insuranceclaim.repository.ClaimRepository;
+import com.sparta.insuranceclaim.service.UserClaimStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    private UserClaimStatusService userClaimStatusService;
 
     @GetMapping("/login")
     public String login(@RequestParam(name="logout", required = false) String loggedOut
@@ -24,7 +35,18 @@ public class LoginController {
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/homepage/user")
-    public String userHomepage() {
+    public String userHomepage(Model model, Authentication authentication) {
+        User loggedInUser = (User)authentication.getPrincipal();
+        List<Claim> submittedClaims = userClaimStatusService.getAllClaimByLoggedInUser(loggedInUser);
+        boolean showNotification = false;
+        for(Claim claim : submittedClaims) {
+            if(claim.getClaimStatus().equals("approved")) {
+                showNotification = true;
+                break;
+            }
+        }
+        showNotification = true;
+         model.addAttribute("showNotification",showNotification);
         return "homepage";
     }
 
